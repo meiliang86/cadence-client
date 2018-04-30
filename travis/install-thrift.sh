e820d719a58d7dfe11da4bd590e3b4db914c4f1f
Liang Mei <meiliang@uber.com>
2018-04-30 12:34:09 -0700

    update travis file

---
+++ travis/install-thrift.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+set -x
+
+THRIFT_VER=0.9.3
+mkdir -p "$THRIFT_PREFIX"
+
+wget http://archive.apache.org/dist/thrift/${THRIFT_VER}/thrift-${THRIFT_VER}.tar.gz
+tar -xzvf thrift-${THRIFT_VER}.tar.gz
+cd thrift-${THRIFT_VER}
+./configure --prefix="$THRIFT_PREFIX" --enable-libs=no --enable-tests=no --enable-tutorial=no
+make -j2 && make install
